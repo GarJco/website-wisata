4b7bad179fe86341b7ba551515b16d6accc83a71
Gar_jco <garrethjeco1@gmail.com>
2025-11-04 18:13:06 +0700

    wisata-web

---
+++ script.js
@@ -0,0 +1,10 @@
+// Menjalankan kode ini setelah halaman web selesai dimuat
+window.onload = function() {
+    
+    // Menampilkan kotak pop-up sapaan
+    alert("Selamat Datang di Pesona Indonesia! 🇮🇩");
+
+    // Anda juga bisa menambahkan fungsionalitas lain di sini.
+    // Contoh: Mencetak pesan ke konsol browser (buka dengan F12)
+    console.log("Halaman berhasil dimuat.");
+};
